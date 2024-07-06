e3d9b447ba005f9024a32868408268f7a8a4907c
souvik gayen <souvikgayen@souviks-MacBook-Air.local>
2024-07-06 15:40:50 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,34 +1,86 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.google.common.base.Equivalence.Wrapper;
+
+import java.time.Duration;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    private Wrappers Wrappers;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
+    @Test
+    public void testCase01() {
+        try {
+            Wrappers.navigateTo("https://forms.gle/wjPkzeSEk1CM7KgGA");
+            Wrappers.waitForPageToLoad();
+            Thread.sleep(3000);
+
+            Wrappers.sendKeys(By.xpath("//input[@type='text']"), "Crio Learner");
+
+            long epochTime = System.currentTimeMillis();
+            Wrappers.sendKeys(By.xpath("//textarea[@aria-label='Your answer']"), "I want to be the best QA Engineer!" + epochTime);
+
+            Wrappers.click(By.xpath("(//div[@id='i13'])[1]"));
+
+            Wrappers.click(By.xpath("(//div[@id='i30'])[1]"));
+            Wrappers.click(By.xpath("(//div[@id='i33'])[1]"));
+            Wrappers.click(By.xpath("(//div[@id='i39'])[1]"));
+
+            Wrappers.click(By.xpath("(//div[@jsname='wQNmvb'])[1]"));
+            Thread.sleep(2000);
+            Wrappers.click(By.xpath("(//div[@data-value='Mr'])[2]"));
+
+            Wrappers.sendKeys(By.xpath("//input[@type='date']"), "28");
+            Wrappers.sendKeys(By.xpath("//input[@type='date']"), "06");
+            Wrappers.sendKeys(By.xpath("//input[@type='date']"), "2024");
+
+            Wrappers.sendKeys(By.xpath("(//input[@type='number'])[1]"), "7");
+            Wrappers.sendKeys(By.xpath("(//input[@type='number'])[2]"), "30");
+
+            Wrappers.click(By.xpath("//div[@role='button'][1]"));
+            Thread.sleep(3000);
+
+            WebElement confirmMessage = Wrappers.findElement("//div[@class='vHW8K']");
+            if(confirmMessage.isDisplayed()){
+                System.out.println(confirmMessage.getText());
+            }
+
+            // Thread.sleep(100000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -42,18 +94,19 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
+
+        Wrappers = new Wrappers(driver, Duration.ofSeconds(10));
 
         driver.manage().window().maximize();
     }
 
     @AfterTest
-    public void endTest()
-    {
-        driver.close();
-        driver.quit();
+    public void endTest() {
+        Wrappers.close();
+        Wrappers.quit();
 
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,9 +3,13 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,4 +20,71 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public Wrappers(WebDriver driver, Duration timeout) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, timeout);
+    }
+
+    public void waitForPageToLoad() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+    }
+
+    public void navigateTo(String url) {
+        driver.get(url);
+        waitForPageToLoad();
+    }
+
+    public void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+    public void sendKeys(By locator, String text) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public String getText(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).getText();
+    }
+
+    public String getTitle() {
+        return driver.getTitle();
+    }
+
+    public WebElement findElement(String xpath) {
+        return driver.findElement(By.xpath(xpath));
+    }
+
+    public boolean isElementVisible(By locator) {
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean isElementDisplayed(WebElement element) {
+        try {
+            return element.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    public void quit() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public void close() {
+        driver.close();
+    }
 }
